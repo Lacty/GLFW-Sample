1d2fac39bd70b8a87011eea9c0fa89cf4ea5f0fb
Lacty <akira206@gmail.com>
2015-10-08 11:54:17 +0900

    README 更新

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# GLFW-Sample
+# GLFW を使ったサンプル
+
+## Mac
+1. プロジェクトフォルダに配布されたincludeファイルをコピペ
+2. Search Paths->Header Search Pathsに`include/`を追加
+3. OSXをプロジェクトにコピペ
+4. Build Phases->Link Binary LibrariesからAddを押し以下を追加
+  * IOKit
+  * CoreVideo
+  * Cocoa
+  * OpenGL
+  * libglfw3.a
---
+++ line/line/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  line
+//
+//  Created by Yanai on 2015/10/07.
+//  Copyright © 2015年 Yanai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
