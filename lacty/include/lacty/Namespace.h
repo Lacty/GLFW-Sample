9db1282c192ddffd8f35bdef59c093f5187d0671
Lacty <akira206@gmail.com>
2015-10-20 11:40:58 +0900

    Namespace.h追加 Vector変更

---
+++ lacty/include/lacty/Namespace.h
@@ -0,0 +1,5 @@
+
+#pragma once
+
+#define LACTY_SCOPE() namespace lacty {
+#define LACTY_END()   }
--- lacty/include/lacty/Vector.hpp
+++ lacty/include/lacty/Vector.hpp
@@ -1,14 +1,16 @@
 
+#pragma once
+#include <lacty/Namespace.h>
 #include <GLFW/glfw3.h>
 
 
-namespace lacty {
+LACTY_SCOPE()
 
 template<typename T>
 class Vec2 {
 public:
   T x, y;
-  
+
   Vec2() : x(0), y(0) {}
   Vec2(T x, T y) : x(x), y(y) {}
 };
@@ -17,4 +19,4 @@ typedef Vec2<int>    Vec2i;
 typedef Vec2<float>  Vec2f;
 typedef Vec2<double> Vec2d;
 
-}
+LACTY_END()
