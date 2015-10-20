b7cc430b74e93395a65236af0c4f199b05fd31d8
Lacty <akira206@gmail.com>
2015-10-20 11:49:05 +0900

    Vector: Vec3追加

--- lacty/include/lacty/Vector.hpp
+++ lacty/include/lacty/Vector.hpp
@@ -15,8 +15,21 @@ public:
   Vec2(T x, T y) : x(x), y(y) {}
 };
 
+
+template<typename T>
+class Vec3 {
+public:
+  T x, y, z;
+  
+  Vec3() : x(0), y(0), z(0) {}
+  Vec3(T x, T y, T z) : x(x), y(y), z(z) {}
+};
+
 typedef Vec2<int>    Vec2i;
 typedef Vec2<float>  Vec2f;
 typedef Vec2<double> Vec2d;
+typedef Vec3<int>    Vec3i;
+typedef Vec3<float>  Vec3f;
+typedef Vec3<double> Vec3d;
 
 LACTY_END()
