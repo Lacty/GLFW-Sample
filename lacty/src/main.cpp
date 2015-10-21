d37f9c6ee47df73fe4d955dbb55fac2b71c67aee
Lacty <akira206@gmail.com>
2015-10-21 15:28:21 +0900

    main: 設計を考察

--- lacty/src/main.cpp
+++ lacty/src/main.cpp
@@ -4,14 +4,48 @@
 */
 
 #include <iostream>
+#include <memory>
+
 #include <GLFW/glfw3.h>
+
 #include <lacty/Vector.h>
 #include <lacty/Color.h>
 
 using namespace lacty;
 
+class AppBase {
+public:
+  AppBase() {}
+  virtual ~AppBase() {}
+  
+  virtual void update() = 0;
+  virtual void draw()   = 0;
+};
+
+class App : public AppBase {
+public:
+  App() {}
+  ~App();
+  
+  void update();
+  void draw();
+};
+
+App::~App() {
+  std::cout << "delete" << std::endl;
+}
+
+void App::update() {
+  std::cout << "update" << std::endl;
+}
+
+void App::draw() {
+  std::cout << "draw" << std::endl;
+}
+
 int main() {
-  Color color;
-  color = Color::orange();
-  std::cout << color.g << std::endl;
+  std::unique_ptr<AppBase> app(new App());
+  
+  app->update();
+  app->draw();
 }
