216ada6c808c03a2204c4973bb19067a098ceb36
Lacty <akira206@gmail.com>
2015-11-11 20:18:22 +0900

    alpha値を使用したサンプル

---
+++ glfw test/src/graph.cpp
@@ -0,0 +1,9 @@
+//
+//  graph.cpp
+//  glfw test
+//
+//  Created by Yanai on 2015/11/11.
+//  Copyright © 2015年 Yanai. All rights reserved.
+//
+
+#include "graph.hpp"
---
+++ glfw test/src/graph.hpp
@@ -0,0 +1,14 @@
+//
+//  graph.hpp
+//  glfw test
+//
+//  Created by Yanai on 2015/11/11.
+//  Copyright © 2015年 Yanai. All rights reserved.
+//
+
+#ifndef graph_hpp
+#define graph_hpp
+
+#include <stdio.h>
+
+#endif /* graph_hpp */
